cbc87ac4e2e8cfdf37bbe0d6b993265a8180498a
henrikvoncoler <voncoler@tu-berlin.de>
2020-09-22 20:38:07 +0200

    Added start script!

---
+++ start_binaural.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+################################################################
+#
+# Start a 24 channel binaural server and a PD/GEM visualizer.
+#
+# - All processes running in the background.
+# - Can be used with GLOOO_synth.
+#
+################################################################
+
+sclang -u 57121 SC/sc_spat_binaural_SERVER.sc 24 &
+
+
+
+puredata -noaudio -nogui PD/source_visualizer.pd &
